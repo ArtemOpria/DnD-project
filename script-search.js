955c9d2c8cfbc728bafb8cde60b00626de289f01
P4nStep4n <malyshko.alex17@ukr.net>
2023-05-10 22:43:50 +0300

    Фікс бестіарію

--- script-search.js
+++ script-search.js
@@ -1,10 +1,11 @@
-const searchInput = document.querySelector('.search-input');
-const monsters = document.querySelectorAll('.pointer');
+const searchInput = document.querySelector('#search');
+const monsters = document.querySelectorAll('.monster');
 
 searchInput.addEventListener('input', () => {
   const searchText = searchInput.value.toLowerCase();
+
   monsters.forEach(monster => {
-    const name = monster.querySelector(".besiary-element").textContent.toLowerCase();
+    const name = monster.querySelector('.name').textContent.toLowerCase();
 
     if (name.includes(searchText)) {
       monster.style.display = '';
